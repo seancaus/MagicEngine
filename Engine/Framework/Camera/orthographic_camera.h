04a0cdda74d82fd859ab799060c80ae45efb7a63
huxf <zivhoo@icloud.com>
2016-07-19 18:17:14 +0800

    no message

---
+++ Engine/Framework/Camera/orthographic_camera.h
@@ -0,0 +1,16 @@
+//
+// Created by huxf on 2016/7/19.
+//
+
+#ifndef MAGICCUBE_ORTHOGRAPHICCAMERA_H
+#define MAGICCUBE_ORTHOGRAPHICCAMERA_H
+
+#include "camera.h"
+
+class OrthographicCamera :public  Camera
+{
+
+};
+
+
+#endif //MAGICCUBE_ORTHOGRAPHICCAMERA_H
