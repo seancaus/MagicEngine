765a2be3bde6ee56494998e1008189e01f140ea4
huxf <zivhoo@icloud.com>
2016-08-05 18:20:05 +0800

    push

---
+++ Engine/Framework/vertex_data.cpp
@@ -0,0 +1,10 @@
+//
+// Created by huxf on 2016/8/5.
+//
+
+#include "vertex_data.h"
+
+namespace Magic
+{
+
+}
---
+++ Engine/Framework/vertex_data.h
@@ -0,0 +1,18 @@
+//
+// Created by huxf on 2016/8/5.
+//
+
+#ifndef MAGIC_VERTEX_DATA_H
+#define MAGIC_VERTEX_DATA_H
+
+namespace Magic
+{
+    class VertexData
+    {
+    public:
+        VertexData();
+        virtual ~VertexData();
+    };
+}
+
+#endif //MAGIC_VERTEX_DATA_H
