489be1d6517128656f8f37a4ae38ee9d76ac53ad
huxf <zivhoo@icloud.com>
2016-08-09 23:23:48 +0800

    no message

---
+++ Engine/Framework/vertex_buffer.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Ziv on 16/8/9.
+//
+
+#include "vertex_buffer.h"
+
+namespace Magic
+{
+
+    VertexBuffer::VertexBuffer() {
+
+    }
+
+    VertexBuffer::~VertexBuffer() {
+
+    }
+
+
+}
---
+++ Engine/Framework/vertex_buffer.h
@@ -0,0 +1,26 @@
+//
+// Created by Ziv on 16/8/9.
+//
+
+#ifndef MAGIC_VERTEX_BUFFER_H
+#define MAGIC_VERTEX_BUFFER_H
+
+#include <GL/glew.h>
+
+namespace Magic
+{
+
+    class VertexBuffer
+    {
+    public:
+        VertexBuffer();
+        ~ VertexBuffer();
+
+    protected:
+
+        GLuint bufferId_;
+
+    };
+}
+
+#endif //MAGIC_VERTEX_BUFFER_H
